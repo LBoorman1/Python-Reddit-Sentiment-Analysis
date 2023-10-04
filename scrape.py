4f4a4a0b6e49143a5b42f58ad3ec028b38d4b9b6
LukeBWood <boormanluke76@gmail.com>
2023-10-04 20:30:04 +0100

    added search term to the program, now allows to see popularity of search term through time averaged over months

--- scrape.py
+++ scrape.py
@@ -7,6 +7,15 @@ import nltk  # natural language tool kit
 import praw  # reddit API wrapper for python
 import credentials  # file containing keys and sensitive data
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import datetime
+import matplotlib.pyplot as plt
+
+
+class SearchResult:
+    def __init__(self, title, date):
+        self.title = title
+        self.date = date
+
 
 user_name = credentials.REDDIT_USERNAME
 user_agent = f"Scraper 1.0 by {user_name}"
@@ -17,21 +26,48 @@ reddit = praw.Reddit(
 )
 
 headlines = set()
-for submission in reddit.subreddit('politics').hot(limit=25):
-    headlines.add(submission.title)
-
-dataFrame = pd.DataFrame(headlines)
-
-dataFrame.to_csv('headlines.csv', header=False, encoding='utf-8', index=False)
+for submission in reddit.subreddit('politics').search('Joe Biden'):
+    obj = SearchResult(
+        submission.title, datetime.datetime.fromtimestamp(submission.created_utc))
+    headlines.add(obj)
 
 sia = SIA()
 results = []
 
-for line in headlines:
-    polarity_score = sia.polarity_scores(line)
-    polarity_score['headline'] = line
+for obj in headlines:
+    polarity_score = sia.polarity_scores(obj.title)
+    polarity_score['headline'] = obj.title
+    polarity_score['date'] = obj.date
     results.append(polarity_score)
 
 dataFrame = pd.DataFrame.from_records(results)
 
-print(dataFrame.head())
+# making an average sentiment over each month
+dateVsScore = dict()
+
+for record in dataFrame.index:
+    compoundScore = dataFrame['compound'][record]
+    yearMonth = dataFrame['date'][record].to_period('M')
+    if (yearMonth not in dateVsScore):
+        dateVsScore[yearMonth] = [compoundScore, 1]
+    else:
+        dateVsScore[yearMonth] = [dateVsScore[yearMonth]
+                                  [0] + compoundScore, dateVsScore[yearMonth][1] + 1]
+
+for key in dateVsScore:
+    dateVsScore[key] = [dateVsScore[key][0] / dateVsScore[key][1]]
+
+df = pd.DataFrame.from_dict(
+    dateVsScore, orient='index', columns=['Score'])
+
+df.index = df.index.to_series().astype(str)
+
+print(df.index)
+
+plt.plot(df.index, df.Score)
+plt.show()
+
+# plt.plot(dataFrame.date, dataFrame.compound)
+# plt.show()
+
+# print(dataFrame.head())
